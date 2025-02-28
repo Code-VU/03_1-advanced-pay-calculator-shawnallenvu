e61e50615a12657a5f1be3eb2d8ea86d86e11a38
Shawn Allen <Shawn.Allen@vu.com>
2025-02-28 14:31:05 -0600

    Passes all 4 tests. Added in try except for bad values. Added overtime calculations

--- payCalculator.py
+++ payCalculator.py
@@ -1,7 +1,33 @@
 def calculatePay():
-    # This first line is provided for you
-    hrs = input("Enter Hours:")
-    
+    # Adding in overtime values 
+    overtimeModifier = 1.5
+    overtimeHours = 0
+
+    # Try except for wrong inputs on hours
+    try:
+        hrs = float(input("Enter Hours: ")) # converting to a float
+    except:
+        print("Error, please enter numeric input")
+        return
+
+    # Try except for wrong inputs on hourly rate
+    try:
+        rate = float(input("Enter Rate: ")) # converting to a float
+    except:
+        print("Error, please enter numeric input")
+        return
+
+    # Calculates how many overtime hours there are are sets normal hours to 40 if overtime is present
+    if hrs > 40:
+        overtimeHours = hrs - 40
+        hrs = 40
+
+    overtimeRate = overtimeModifier * rate # Calculating overtime rate based on the modifier defined above
+
+    gross_pay = overtimeHours * overtimeRate + hrs * rate # Calculating gross pay based on normal and overtime hours and rates
+
+    print(f"Pay: {gross_pay}")
+
     # end assignment
 
 ## if you want to test locally before you try to sync
